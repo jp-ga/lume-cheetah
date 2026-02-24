240ed92d830dd6119d56e8c2fdeb473a319cb705
Chris Garnier <cgarnier@slac.stanford.edu>
2026-02-24 15:44:51 -0800

    removed SLACCheetahTransformer, it is moving to the VA

--- lume_cheetah/transformer.py
+++ lume_cheetah/transformer.py
@@ -21,7 +21,7 @@ class CheetahTransformer(ABC):
     """
 
     def __init__(self, control_name_to_cheetah: dict[str, str]):
-        self.control_name_to_cheetah = control_name_to_cheetah
+        self._control_name_to_cheetah = control_name_to_cheetah
 
     @property
     def control_name_to_cheetah(self):
@@ -59,58 +59,3 @@ class CheetahTransformer(ABC):
         pass
 
 
-class SLACCheetahTransformer(CheetahTransformer):
-    """
-    CheetahTransformer subclass for SLAC accelerator simulations.
-
-    This class can be extended to include any necessary unit conversions or
-    special handling for SLAC-specific control variables and their mapping
-    to cheetah properties.
-    """
-
-    def get_cheetah_property(self, simulator, control_name, energy):
-        """
-        Get a property of a Cheetah element based on the control
-        variable name and return its value in EPICS units.
-
-        Parameters
-        ----------
-        simulator : CheetahSimulator
-            The simulator instance containing the segment and elements.
-        control_name : str
-            The name of the control variable (e.g. "QUAD:Q1:B1_GRAD")
-        energy : float
-            The beam energy in eV, used for unit conversions if necessary.
-        """
-        cheetah_mapping = self.control_name_to_cheetah.get(control_name)
-        if cheetah_mapping is None:
-            raise ValueError(f"No mapping found for control variable '{control_name}'")
-
-        element_name, attribute = cheetah_mapping.split()
-        element = getattr(simulator.segment, element_name)
-        return access_cheetah_attribute(element, attribute, energy)
-
-    def set_cheetah_property(self, simulator, control_name, value, energy):
-        """
-        Set a property of a Cheetah element based on the control variable
-        name and value in EPICS units.
-
-        Parameters
-        ----------
-        simulator : CheetahSimulator
-            The simulator instance containing the segment and elements.
-        control_name : str
-            The name of the control variable (e.g. "QUAD:Q1:B1_GRAD")
-        value : Any
-            The value to set for the corresponding cheetah property, in EPICS units.
-        energy : float
-            The beam energy in eV, used for unit conversions if necessary.
-        """
-
-        cheetah_mapping = self.control_name_to_cheetah.get(control_name)
-        if cheetah_mapping is None:
-            raise ValueError(f"No mapping found for control variable '{control_name}'")
-
-        element_name, attribute = cheetah_mapping.split()
-        element = getattr(simulator.segment, element_name)
-        access_cheetah_attribute(element, attribute, energy, set_value=value)
