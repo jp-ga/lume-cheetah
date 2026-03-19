2cee32a0b12bba23b1c741b46faf80112e414caf
Chris Garnier <cgarnier@slac.stanford.edu>
2026-03-19 11:36:36 -0700

    updated pg to particle beam function before using it in simulator object

--- lume_cheetah/utils.py
+++ lume_cheetah/utils.py
@@ -1,6 +1,18 @@
 import cheetah
 from pmd_beamphysics import ParticleGroup
+import torch
 
-def particlegroup_to_cheetah_beam(pg: ParticleGroup):
+def particlegroup_to_cheetah_beam(
+    pg: ParticleGroup,
+    dtype: torch.dtype = torch.float32,
+    device: torch.device | str | None = None,
+):
     """Convert an openPMD-beamphysics ParticleGroup to a Cheetah ParticleBeam."""
-    return cheetah.ParticleBeam.from_openpmd_particlegroup(pg)
+    energy = torch.as_tensor(pg["energy"], dtype=dtype, device=device).mean()
+
+    return cheetah.ParticleBeam.from_openpmd_particlegroup(
+        particle_group=pg,
+        energy=energy,
+        dtype=dtype,
+        device=device,
+    )
